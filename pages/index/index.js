a14b10b083f11e0bf38622b07fef1fa61e909fdc
dz980972669 <dz980972669@163.com>
2019-06-12 15:48:39 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -40,7 +40,13 @@ Page({
       storeid: app.globalData.storeid, 
     })
 
-    
+    /* 查看本地是否有用户信息,没有则重新授权 */
+    const userInfo = wx.getStorageSync('userInfo');
+    if (!userInfo) {
+      this.setData({
+        unAuth: true
+      })
+    }
     wx.showLoading({
       title: '加载中',
       mask: true
@@ -56,6 +62,37 @@ Page({
         shopList: res.datas,
       })
       wx.hideLoading();
+    })
+  },
+  // 用户授权
+  onUserInfo(e) {
+    // 获取用户信息
+    const userInfo = e.detail.userInfo;
+    // 存入本地
+    wx.setStorageSync('userInfo', userInfo);
+    // 存入用户详细信息
+    wx.setStorageSync('userInfoDetail', e.detail);
+
+    // 存入全局
+    app.globalData.userInfo = userInfo;
+    this.setData({
+      unAuth: false
+    })
+  },
+  // 显示时授权窗口判断
+  onShow() {
+    // 显示时，如果用户授权了。则隐藏授权窗口
+    const userInfo = wx.getStorageSync('userInfo');
+    if (userInfo) {
+      this.setData({
+        unAuth: false
+      })
+    }
+  },
+  // 取消
+  onCancel(e) {
+    this.setData({
+      unAuth: false
     })
   },
   
